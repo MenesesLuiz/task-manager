089d2f9e139ce6ff1fc546a68322d96764d81230
LucaasHenrique <lucashsouza101@gmail.com>
2025-05-11 17:17:17 -0300

    feat: add database

--- auth.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController.js')
-
-router.post('/registrar', authController.registrarUsuario);
-router.post('/login', authController.loginUsuario);
-
-module.exports = router;
