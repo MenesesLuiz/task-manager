15a3b0a51dc2bd8f94ebf360de500b5e1b43a5ad
LucaasHenrique <lucashsouza101@gmail.com>
2025-05-13 10:26:59 -0300

    feat: add metodo checkPassword no model usuario

--- models/Usuario.js
+++ models/Usuario.js
@@ -1,37 +1,50 @@
-const { DataTypes, Model } = require('sequelize');
+const { DataTypes, Model, Sequelize } = require('sequelize');
+const bcrypt = require('bcrypt');
 
 class Usuario extends Model {
     static init(sequelize) {
         super.init({
             id: {
-                type: DataTypes.UUID,
+                type: DataTypes.INTEGER,
                 primaryKey: true,
-            },
+                autoIncrement: true
+            },  
             nome: {
                 type: DataTypes.STRING,
                 allowNull: false
-
             },
             email: {
                 type: DataTypes.STRING,
                 allowNull: false,
                 unique: true
             },
-            senha:  {
-                type: DataTypes.STRING,
-                allowNull: false
+            senha: {
+                type: Sequelize.VIRTUAL
+            },
+            senha_hash:  {
+                type: DataTypes.STRING, 
             }, 
         }, {
             sequelize,
-            tableName: {
-                singular: 'usuario',
-                plural: 'usuarios'
-            } 
+            modelName: 'Usuario',
+            tableName: 'usuarios'
         });
+
+        this.addHook('beforeSave', async usuario => {
+            if (usuario.senha) {
+                usuario.senha_hash = await bcrypt.hash(usuario.senha, 8);
+            }
+        });
+
+        return this;
+    }
+
+    checkPassword(senha) {
+       return bcrypt.compare(senha, this.senha_hash);
     }
 
     static associate(models) {
-        this.hasMany(models.Tarefas);
+        this.hasMany(models.Tarefa);
     }
 }
 
