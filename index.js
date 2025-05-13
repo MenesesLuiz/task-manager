1c88c30a1b1978552cce0eaf237c0bbaa556efc2
LucaasHenrique <lucashsouza101@gmail.com>
2025-05-13 10:28:34 -0300

    index.js

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const tarefasRoutes = require('./routes/tarefas.js');
 const authRoutes = require('./routes/auth.js');
 const database = require('./config/index.js');
 
-database;
-
 app.use(express.json());
 app.use('/auth', authRoutes);
 app.use('/tarefas', tarefasRoutes);
 
-app.listen(3000, () => {
-  console.log('Servidor rodando em http://localhost:3000');
+PORT = 8080;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
